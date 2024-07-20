ed5e586d553452d01e11b1acbe22c93a11dd206d
林才 <god_lin@foxmail.com>
2024-07-20 13:44:34 +0800

    add index page!

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask import url_for
 from markupsafe import escape # 防止恶意代码注入
 
 app=Flask(__name__)
 
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+
+
 @app.route("/user/<name>")
 def user(name):
     return f'<h1>Hello {escape(name)}!</h1><img src="http://helloflask.com/totoro.gif">'
 
+@app.route("/")
+def  index():
+    return render_template('index.html', name=name, movies=movies) # 传入变量  默认从templates里面找
 
 @app.route("/hello")
 def hello():
